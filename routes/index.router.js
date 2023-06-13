ee766e9e437fe1ef03100936e15d727a4c7e906b
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-06-13 18:06:19 +0300

    finish routes

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,6 +1,11 @@
+const permissionRouter = require('./permissions.router');
+const productRouter = require('./product.router');
+const userRouter = require('./user.router');
+
 const mainRouter = require('express').Router();
 
-
-
+mainRouter.use('/products', productRouter);
+mainRouter.use('/users', userRouter);
+mainRouter.use('/permissions', permissionRouter);
 
 module.exports = mainRouter;
